4eda25005e0082a38783aa951bafdfa61750fb30
Saurabh Giri <np03a190090@heraldcollege.edu.np>
2021-12-13 09:21:55 +0545

    files in main.dart is updated

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -9,11 +9,10 @@ class MyApp extends StatelessWidget{
       home: Scaffold(
         appBar: AppBar(
           title: Text('UMS'),
-      ),
+        ),
         body: Column(
           children: [
             Text('Login with your credential'),
-
           ],
         ),
       )
